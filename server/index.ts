a335ffdbcc0d0d88ce201b3a5d84507915e25c84
RAHUL YADAV <rahulyadavmlsu2130@gmail.com>
2025-08-30 16:21:01 +0530

    Update index.ts

--- server/index.ts
+++ server/index.ts
@@ -61,6 +61,6 @@ app.use((req, res, next) => {
   // this serves both the API and the client.
   // It is the only port that is not firewalled.
   const port = parseInt(process.env.PORT || '5000', 10);
-server.listen(port, "127.0.0.1", () => {
+server.listen(port, "0.0.0.0", () => {
   log(`serving on http://127.0.0.1:${port}`);
 });})();
